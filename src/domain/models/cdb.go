095059c4566313b984a75df685c4b00763638d81
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2024-05-29 14:22:26 -0300

    Creating update CBD job

--- src/domain/models/cdb.go
+++ src/domain/models/cdb.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"math"
+	"time"
+)
 
 type CdbType string
 
@@ -18,10 +22,28 @@ type Cdb struct {
 	Type       CdbType   `json:"type" validate:"required"`
 }
 
-func (c *Cdb) Update(cdis []Cdi, orders []CdbOrder) {
+func (c *Cdb) Update(cdis []Cdi, orders []CdbOrder, until time.Time) {
 	c.Amount = 0
-	for _, order := range orders {
-		c.Amount += order.Amount
+	if len(orders) == 0 {
+		return
+	}
+	lastOrder := orders[0]
+	c.Amount = lastOrder.Amount
+	if len(orders) == 1 {
+		for _, cdi := range cdis {
+			c.Amount = roundFloat(c.Amount+(c.Amount*(cdi.Rate/100)*(c.Percentage/100)), 2)
+		}
+		return
+	}
+	for i, order := range orders {
+		if i > 0 {
+			c.Amount += order.Amount
+		}
 	}
 
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
--- test/domain/models/cdb_test.go
+++ test/domain/models/cdb_test.go
@@ -11,12 +11,12 @@ func TestCdbUpdate(t *testing.T) {
 	cdb := &models.Cdb{
 		UserID:     "123",
 		Amount:     0,
-		Percentage: 0,
+		Percentage: 100,
 		Type:       models.CdbInvestmentTypeCdi,
 	}
 
 	t.Run("Should update CDB amount when no orders created", func(t *testing.T) {
-		cdb.Update([]models.Cdi{}, []models.CdbOrder{})
+		cdb.Update([]models.Cdi{}, []models.CdbOrder{}, time.Now())
 
 		assert.Equal(t, float64(0), cdb.Amount)
 	})
@@ -26,7 +26,7 @@ func TestCdbUpdate(t *testing.T) {
 			UserID: "123",
 			Type:   models.CdbOrderTypeBuy,
 			Amount: 155.55,
-		}})
+		}}, time.Now())
 
 		assert.Equal(t, 155.55, cdb.Amount)
 	})
@@ -37,9 +37,41 @@ func TestCdbUpdate(t *testing.T) {
 			Type:   models.CdbOrderTypeBuy,
 			Amount: 155.55,
 			Date:   time.Now(),
-		}})
+		}}, time.Now())
 
 		assert.Equal(t, 155.55, cdb.Amount)
+	})
+
+	t.Run("Should update CDB amount when two purchases created and no CDI info", func(t *testing.T) {
+		cdb.Update([]models.Cdi{}, []models.CdbOrder{
+			{
+				UserID: "123",
+				Type:   models.CdbOrderTypeBuy,
+				Amount: 155.55,
+				Date:   time.Now(),
+			},
+			{
+				UserID: "123",
+				Type:   models.CdbOrderTypeBuy,
+				Amount: 155.55,
+				Date:   time.Now(),
+			}}, time.Now())
+
+		assert.Equal(t, 311.10, cdb.Amount)
+	})
+
+	t.Run("Should update CDB amount when position is next month", func(t *testing.T) {
+		cdb.Update([]models.Cdi{models.Cdi{
+			Rate: 0.68591,
+			Date: time.Date(2024, 05, 24, 12, 12, 12, 0, time.UTC),
+		}}, []models.CdbOrder{models.CdbOrder{
+			UserID: "123",
+			Type:   models.CdbOrderTypeBuy,
+			Amount: 90940.38,
+			Date:   time.Now(),
+		}}, time.Date(2024, 05, 01, 12, 12, 12, 0, time.UTC))
+
+		assert.Equal(t, 91564.15, cdb.Amount)
 	})
 
 }
